a0f8789db712efe84a7087a7cb98e744a746af2b
Mateo Rios Diaz <mateorios993@gmail.com>
2024-06-09 22:21:51 -0500

    Se agrega dependencia de icons de primeng y se realiza maquetacion inicial del form para los expenses

--- src/app/modules/expenses/page/expenses.component.ts
+++ src/app/modules/expenses/page/expenses.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-expenses',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CalendarModule, DropdownModule, InputNumberModule, InputTextModule],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.css'
 })
 export class ExpensesComponent {
 
+  saveExpense(){}
 }
