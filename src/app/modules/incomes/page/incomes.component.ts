5f9e564b7a1b2db3ff2b31a4e6f97fac42fb6c87
Mateo Rios Diaz <mateorios993@gmail.com>
2024-06-10 10:34:30 -0500

    Se realiza maquetacion income module

--- src/app/modules/incomes/page/incomes.component.ts
+++ src/app/modules/incomes/page/incomes.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { Income } from '../models/income';
+import INCOMES from '../../../data/incomes.json'
 
 @Component({
   selector: 'app-incomes',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CalendarModule, DropdownModule, InputNumberModule, InputTextModule],
   templateUrl: './incomes.component.html',
   styleUrl: './incomes.component.css'
 })
 export class IncomesComponent {
+  data: Income[] = INCOMES.incomes
 
+  saveIncome() { }
 }
